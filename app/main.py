81bc52d043fea1a850f792839e599eec9c54ab8f
Mazurkav <aleshkamazurkevich@gmail.com>
2024-12-12 22:08:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,14 +3,19 @@
 def get_coin_combination(cents: int) -> list:
     """
     Calculate the smallest combination of coins for a given amount in cents.
-    Returns a list: [pennies, nickels, dimes, quarters].
+
+    The function returns a list where:
+    - coins[0] = number of pennies (1 penny = 1 cent)
+    - coins[1] = number of nickels (1 nickel = 5 cents)
+    - coins[2] = number of dimes (1 dime = 10 cents)
+    - coins[3] = number of quarters (1 quarter = 25 cents)
 
     Args:
         cents (int): The total amount in cents to convert into coins.
 
     Returns:
-        list: A list of the smallest combination of coins
-              [pennies, nickels, dimes, quarters].
+        list: A list representing the smallest combination of coins for the given
+              amount in cents. The list contains [pennies, nickels, dimes, quarters].
     """
     values = [1, 5, 10, 25]
     coins = [0, 0, 0, 0]
